b09edc2a04fc7a6531d466c3b49e6b8f46564b7f
MattCowley <matthew@cowley.org.uk>
2016-02-13 12:23:52 +0000

    loader

--- src/loader.js
+++ src/loader.js
@@ -7,37 +7,43 @@
  */
 
 //Declare Version
-var LoaderVersion = "1.06";
+var LoaderVersion = "1.07";
 
 function loader(scripts, loadedCallback) {
 	var scriptURLS = {
-		"colorConsole": "//rawgit.com/MattCowley/MagicBootTabs/master/src/colorConsole.js",
-		"MagicBootTabs": "//rawgit.com/MattCowley/MagicBootTabs/master/src/jquery.magicBootTabs.js",
-		"jQuery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js",
-		"bootStrap": "//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"
+		"colorConsole": Array("script", "//rawgit.com/MattCowley/MagicBootTabs/master/src/colorConsole.js"),
+		"MagicBootTabs": Array("script", "//rawgit.com/MattCowley/MagicBootTabs/master/src/jquery.magicBootTabs.js"),
+		"jQuery": Array("script", "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"),
+		"bootStrapJS": Array("script", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"),
+		"bootStrapCSS": Array("style", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
 	}
 
 	var scriptCount = scripts.length;
 
-	function loadScript(url, callback) {
-	    // Adding the script tag to the head as suggested before
+	function load(url, type, callback) {
 	    var head = document.getElementsByTagName('head')[0];
-	    var script = document.createElement('script');
-	    script.type = 'text/javascript';
-	    script.src = url;
+	    var item;
 
-	    // Then bind the event to the callback function.
-	    // There are several events for cross browser compatibility.
-	    script.onreadystatechange = callback;
-	    script.onload = callback;
-
-	    // Fire the loading
-	    head.appendChild(script);
+	    if (type == "script") {
+		    item = document.createElement('script');
+		    item.type = 'text/javascript';
+		    item.src = url;
+		}
+		if (type == "style") {
+		    item = document.createElement('link');
+		    item.rel = 'stylesheet';
+		    item.href = url;
+		}
+	    
+	    item.onreadystatechange = callback;
+	    item.onload = callback;
+	    head.appendChild(item);
 	}
 	function loadNext(index) {
 		if (index < scriptCount) {
 			loadScript(
-				scriptURLS[scripts[index]],
+				scriptURLS[scripts[index][1]],
+				scriptURLS[scripts[index][0]],
 				function(){ loadNext(index+1); }
 			);
 		} else {
